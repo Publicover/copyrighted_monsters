0bdd2f3c50bc19faaecdd81f8984c1850b64bc8c
Publicover <jameslpublicover@gmail.com>
2016-07-13 23:39:04 -0400

    do Monster before_save set_rarity as a case statement

--- app/models/monster.rb
+++ app/models/monster.rb
@@ -3,10 +3,33 @@ class Monster < ActiveRecord::Base
   belongs_to :location
 
   def set_rarity
-    if self.name == "Abra"
-      self.rarity = "It worked!"
+    case self.name
+    when "Rattata", "Pidgey", "Weedle", "Venonat", "Zubat", "Caterpie", "Doduo"
+      self.rarity = "0: Everywhere"
+    when "Oddish", "Paras", "Gastly", "Drowzee", "Eevee", "Magikarp", "Meowth",
+         "Bellsprout", "Ekans", "Nidoran M/F", "Polywag", "Slowpoke", "Goldeen",
+         "Krabby", "Mankey"
+      self.rarity = "1: Common"
+    when "Spearow", "Growlith", "Jigglypuff", "Cleafairy", "Psyduck", "Seel",
+         "Abra", "Geodude", "Sandshrew", "Vulpix", "Diglett", "Exeggcute",
+         "Tentacool", "Shellder", "Onix", "Poliwhirl"
+      self.rarity = "2: Uncommon"
+    when "Machamp", "Ponyta", "Weezing", "Voltrob", "Horsea", "Staryu",
+         "Tangela", "Cubone", "Ryhorn", "Grimmer", "Magnemite", "Scyther",
+         "Lickitung"
+      self.rarity = "3: Rare"
+    when "Bulbasaur", "Charmander", "Squirtle", "Pikachu", "Omanyte", "Kabuto",
+         "Aerodactyl", "Hitmochan", "Hitmonlee"
+      self.rarity = "4: Special"
+    when "Dratini", "Farfetch'd", "Mr Mine", "Jynx", "Magmar", "Electabuzz",
+         "Porygon"
+      self.rarity = "5: Epic"
+    when "Lapras", "Snorlax", "Kangaskhan", "Chansey"
+      self.rarity = "6: Myths"
+    when "Ditto"
+      self.rarity = "7: Still not convinced it exists"
     else
-      self.rarity = "The else worked too!"
+      self.rarity = "I have no idea"
     end
   end
 end
