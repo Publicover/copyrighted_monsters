4ea5bf597fe3e5c17e7e7b2d4b74c926b1cb7e7e
Publicover <jameslpublicover@gmail.com>
2016-07-14 10:15:37 -0400

    get monsters index to show monsters

--- app/controllers/monsters_controller.rb
+++ app/controllers/monsters_controller.rb
@@ -1,4 +1,9 @@
 class MonstersController < ApplicationController
+  before_filter :load_location
+
+  def index
+    @monsters = @location.present? ? @location.monsters : Monster.all
+  end
 
   def create
     @location = Location.find(params[:location_id])
@@ -7,6 +12,10 @@ class MonstersController < ApplicationController
   end
 
   private
+    def load_location
+      @location = Location.find(params[:location_id]) if params[:location_id].present?
+    end
+  
     def monster_params
       params.require(:monster).permit(:name, :rarity)
     end
