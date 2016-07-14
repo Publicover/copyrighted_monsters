b31197e2841bc82f3ad2455ea86de63a1dd85e74
Publicover <jameslpublicover@gmail.com>
2016-07-13 23:07:11 -0400

    give Monster a before_save that sets rarity depending on name picked.

--- app/models/monster.rb
+++ app/models/monster.rb
@@ -1,3 +1,12 @@
 class Monster < ActiveRecord::Base
+  before_save :set_rarity
   belongs_to :location
+
+  def set_rarity
+    if self.name == "Abra"
+      self.rarity = "It worked!"
+    else
+      self.rarity = "The else worked too!"
+    end
+  end
 end
